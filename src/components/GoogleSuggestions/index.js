882cf738908dbe7640b8d97a7d86d77d6ca6ea80
pothanna <pothanna.pullolla@gmail.com>
2022-05-15 12:17:46 +0000

    react google sugestion

---
+++ src/components/GoogleSuggestions/index.js
@@ -0,0 +1,66 @@
+import {Component} from 'react'
+
+import SuggestionItem from '../SuggestionItem'
+
+import './index.css'
+
+class GoogleSuggestions extends Component {
+  state = {
+    searchInput: '',
+  }
+
+  updateSearchInput = value => {
+    this.setState({searchInput: value})
+  }
+
+  onChangeInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  render() {
+    const {suggestionsList} = this.props
+    const {searchInput} = this.state
+    const searchResults = suggestionsList.filter(eachItem =>
+      eachItem.suggestion.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+
+    return (
+      <div className="google-app-container">
+        <div className="google-text-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
+            alt="google logo"
+            className="google-image"
+          />
+          <div className="search-input-container">
+            <div className="input-container">
+              <img
+                alt="search icon"
+                className="search-icon"
+                src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
+              />
+              <input
+                type="search"
+                className="search-input"
+                placeholder="Search Google"
+                onChange={this.onChangeInput}
+                value={searchInput}
+              />
+            </div>
+            <ul className="search-list-container">
+              {searchResults.map(eachItem => (
+                <SuggestionItem
+                  suggestionDetails={eachItem}
+                  key={eachItem.id}
+                  updateSearchInput={this.updateSearchInput}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GoogleSuggestions
